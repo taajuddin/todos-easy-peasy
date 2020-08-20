23a080a0077972b10138af6bdd676e9c1de541fe
taajuddin <taajraza@gmail.com>
2020-08-20 22:04:46 +0530

    Updates

---
+++ precache-manifest.b1c8ff88774a068007eea15aa168a349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369c38520410d658c6833e550055b3c9",
+    "url": "/todo-easy-peasy/index.html"
+  },
+  {
+    "revision": "a9aa19bc238c6529cb1c",
+    "url": "/todo-easy-peasy/static/css/main.98f3006d.chunk.css"
+  },
+  {
+    "revision": "9336ddd419bf4cd98627",
+    "url": "/todo-easy-peasy/static/js/2.77b6379f.chunk.js"
+  },
+  {
+    "revision": "a9aa19bc238c6529cb1c",
+    "url": "/todo-easy-peasy/static/js/main.dbece28a.chunk.js"
+  },
+  {
+    "revision": "42c3e6e20be44478c156",
+    "url": "/todo-easy-peasy/static/js/runtime~main.6368447d.js"
+  }
+]);
